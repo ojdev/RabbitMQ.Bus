18bdbd206faffd828fc692a0ed440f545def675b
俊 欧 <dev_ouj@hotmail.com>
2018-06-21 17:56:52 +0800

    修改topic

--- RabbitMQ.Bus/RabbitMQBus.cs
+++ RabbitMQ.Bus/RabbitMQBus.cs
@@ -14,7 +14,6 @@ namespace RabbitMQ.Bus
     {
         private readonly RabbitMQBusFactory _factory;
         private readonly RabbitMQConfig _config;
-        private bool _isBinding = false;
         /// <summary>
         /// 现有队列
         /// </summary>
@@ -38,18 +37,18 @@ namespace RabbitMQ.Bus
         /// <summary>
         /// 订阅消息
         /// </summary>
-        /// <typeparam name="TIn"></typeparam>
-        /// <typeparam name="TOut"></typeparam>
+        /// <typeparam name="TMessage">消息类型</typeparam>
+        /// <typeparam name="THandler">消息处理类型</typeparam>
         /// <param name="queueName">队列名称</param>
         /// <param name="isReply">是否确认消费，确认后其他人将接收不到消息,默认为确认</param>
-        public void Subscribe<TIn, TOut>(string queueName, bool isReply = true) where TIn : IRabbitMQBusHandler<TOut>
+        public void Subscribe<TMessage, THandler>(string queueName, bool isReply = true) where THandler : IRabbitMQBusHandler<TMessage>
         {
             EventingBasicConsumer _consumer = new EventingBasicConsumer(_factory.Channel);
             _consumer.Received += (ch, ea) =>
             {
-                var handler = Activator.CreateInstance<TIn>();
+                var handler = Activator.CreateInstance<THandler>();
                 var message = Encoding.UTF8.GetString(ea.Body);
-                handler.Handle(JsonConvert.DeserializeObject<TOut>(message));
+                handler.Handle(JsonConvert.DeserializeObject<TMessage>(message));
                 if (isReply)
                 {
                     _factory.Channel.BasicAck(ea.DeliveryTag, false);
@@ -60,16 +59,17 @@ namespace RabbitMQ.Bus
         /// <summary>
         /// 发送消息
         /// </summary>
-        /// <typeparam name="TIn"></typeparam>
+        /// <typeparam name="TMessage"></typeparam>
         /// <param name="queueName"></param>
         /// <param name="value"></param>
         /// <param name="routingKey">路由Key，可为空</param>
-        public void Publish<TIn>(string queueName, TIn value, string routingKey = "")
+        public void Publish<TMessage>(string queueName, TMessage value, string routingKey = "")
         {
-            if (!_isBinding || !Queues.Contains(queueName))
+            if (!Queues.Contains(queueName))
             {
                 Binding(queueName, routingKey);
             }
+            
             var sendBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(value));
             _factory.Channel.BasicPublish(_config.ExchangeName, routingKey, null, sendBytes);
         }
@@ -81,10 +81,9 @@ namespace RabbitMQ.Bus
         private void Binding(string queueName, string routingKey = "")
         {
             _factory.Channel.QueueUnbind(queueName, _config.ExchangeName, routingKey);
-            _factory.Channel.ExchangeDeclare(_config.ExchangeName, ExchangeType.Direct, false, false, null);
+            _factory.Channel.ExchangeDeclare(_config.ExchangeName, _config.ExchangeType, false, false, null);
             _factory.Channel.QueueDeclare(queueName, false, false, false, null);
             _factory.Channel.QueueBind(queueName, _config.ExchangeName, routingKey, null);
-            _isBinding = true;
             Queues.Add(queueName);
         }
     }
